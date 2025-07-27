425c8ba2bf31b94e3c3c8ff57bf29c2cfca383f6
Saleh Mohammed Abu Saleh <devsalxh@gmail.com>
2025-07-27 15:43:59 +0300

    Create 6-ZigzagConversion.cs

---
+++ 6-ZigzagConversion.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public string Convert(string s, int numRows) {
+        if (numRows == 1 || s.Length <= numRows)
+            return s;
+
+        List<string> rows = new List<string>(new string[Math.Min(numRows, s.Length)]);
+        int currentRow = 0;
+        bool goingDown = false;
+
+        foreach (char c in s) {
+            rows[currentRow] += c;
+            if (currentRow == 0 || currentRow == numRows - 1)
+                goingDown = !goingDown;
+            currentRow += goingDown ? 1 : -1;
+        }
+
+        return string.Join("", rows);
+    }
+}
